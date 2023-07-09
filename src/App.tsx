f3106039dc1073605ba9d79ca19756237d22c7c4
xion <calf-love@naver.com>
2023-07-10 00:22:39 +0900

    :sparkles: Svgr
    create svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import '@/App.css';
+import { ReactComponent as ReactIcon } from '@/assets/react.svg';
 import React from 'react';
 function App() {
   return (
     <div>
       <React.Suspense fallback={<div>로딩중입니다.</div>}>
         <div>암</div>
+        <ReactIcon />
       </React.Suspense>
     </div>
   );
