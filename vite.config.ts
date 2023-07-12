9a5ca9b5c99a29059edc2049ae6ca0b2f789c30e
xion <calf-love@naver.com>
2023-07-12 23:19:42 +0900

    :sparkles: React-error-boundary and compression
    dep add

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
+import { compression } from 'vite-plugin-compression2';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr(),
+    compression({
+      include: [/\.(js)$/, /\.(css)$/, /\.(webp)$/],
+      threshold: 1400,
+    }),
+  ],
 });
