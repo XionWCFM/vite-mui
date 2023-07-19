b5c44aabfff7171d553311ea20089475132c04ed
xion <calf-love@naver.com>
2023-07-20 00:50:48 +0900

    :sparkles: Error and suspense
    add

---
+++ src/components/error-loading-components/LoadingPage.tsx
@@ -0,0 +1,5 @@
+const LoadingPage = () => {
+  return <div>로딩중이에염</div>;
+};
+
+export default LoadingPage;
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,29 @@
-import Providers from './components/helper-components/Providers.tsx';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { worker } from './server/worker.ts';
-import CssBaseline from '@mui/material/CssBaseline';
 import '@/styles/reset.css';
 import '@/styles/index.css';
 import '@/styles/animation.css';
+
+import Providers from './components/helper-components/Providers.tsx';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import CssBaseline from '@mui/material/CssBaseline';
+import ErrorFallback from './components/error-loading-components/ErrorFallback.tsx';
+
+import { ErrorBoundary } from 'react-error-boundary';
+import { worker } from './server/worker.ts';
+import React from 'react';
+import LoadingPage from './components/error-loading-components/LoadingPage.tsx';
 
 if (process.env.NODE_ENV === 'development') {
   void worker.start();
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Providers>
-    <CssBaseline />
-    <App />
-  </Providers>,
+  <ErrorBoundary fallbackRender={ErrorFallback}>
+    <React.Suspense fallback={<LoadingPage />}>
+      <Providers>
+        <CssBaseline />
+        <App />
+      </Providers>
+    </React.Suspense>
+  </ErrorBoundary>,
 );
