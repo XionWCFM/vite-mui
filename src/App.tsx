e39bd2fe2f6db432c47e344c9def9ed52b309585
xion <calf-love@naver.com>
2023-07-21 05:17:11 +0900

    :test_tube: Test
    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
 import * as S from '@/components/atom-components';
-import { Typography } from '@mui/material';
-import DarkToggleButton from './components/helper-components/DarkToggleButton';
-import React from 'react';
-import axios from 'axios';
-
-const fetcher = async () => {
-  const response = await axios.get<string[]>('/example');
-  return response;
-};
+import { useState } from 'react';
 
 const App = () => {
-  const [data, setData] = React.useState<string[]>();
-  React.useEffect(() => {
-    fetcher()
-      .then((data) => setData(data.data))
-      .catch((error) => console.log(error));
-  }, []);
+  const [counter, setCounter] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(false);
 
   return (
     <S.FullLayout justifyCentered alignCentered>
-      <Typography variant="h3">무언가 잘못되었습니다.</Typography>
-      <Typography variant="h5">잠시 후 다시 시도하세요.</Typography>
-      <div>엄준식</div>
-      <DarkToggleButton>엄준석</DarkToggleButton>
-      <div>{data?.map((e) => <li key={e}>{e}</li>)}</div>
+      <h3 data-testid="counter">{counter}</h3>
+      <button
+        data-testid="minus-button"
+        onClick={() => setCounter((state) => state - 1)}
+        disabled={isDisabled}
+      >
+        -
+      </button>
+      <button
+        data-testid="plus-button"
+        onClick={() => setCounter((state) => state + 1)}
+        disabled={isDisabled}
+      >
+        +
+      </button>
+      <button
+        data-testid="on/off-button"
+        style={{
+          backgroundColor: 'blue',
+        }}
+        onClick={() => setIsDisabled((state) => !state)}
+      >
+        온오프
+      </button>
     </S.FullLayout>
   );
 };
--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,35 +1,54 @@
-import axios from 'axios';
 import App from '../App';
-import { render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 
-jest.mock('axios');
+test('카운터는 0으로 시작해야합니다.', () => {
+  render(<App />);
+  const counterElement = screen.getByTestId('counter');
 
-describe('이게 테스트다', () => {
-  const app = render(<App />);
-
-  test('페이지가 제대로 뜨나요?', () => {
-    const a = app.getByText('엄준식');
-    expect(a).toBeInTheDocument();
-  });
+  expect(counterElement).toHaveTextContent('0');
 });
-// Tests
-// test("Renders main page correctly", async () => {
-//   // Setup
-//   render(<App />);
-//   const buttonCount = await screen.findByRole("button");
-//   const codeCount = await screen.queryByText(/The count is now:/);
 
-//   // Pre Expecations
-//   expect(buttonCount.innerHTML).toBe("count is 0");
-//   // Instead of:
-//   expect(codeCount).toBeNull();
-//   expect(codeCount).not.toBeInTheDocuement();
+test('마이너스버튼이 있어야합니다.', () => {
+  render(<App />);
 
-//   // Init
-//   fireEvent.click(buttonCount);
-//   fireEvent.click(buttonCount);
+  const buttonElement = screen.getByTestId('minus-button');
+  expect(buttonElement).toHaveTextContent('-');
+});
+test('플러스버튼이 있어야합니다.', () => {
+  render(<App />);
 
-//   // Post Expectations
-//   expect(buttonCount.innerHTML).toBe("count is 2");
-//   expect(await screen.queryByText(/The count is now:/)).toBeInTheDocument();
-// });
+  const buttonElement = screen.getByTestId('plus-button');
+  expect(buttonElement).toHaveTextContent('+');
+});
+
+test('플러스 버튼을 클릭하면 값이 올라야합니다.', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('plus-button');
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent('1');
+});
+
+test('마이너스 버튼을 클릭하면 값이 올라야합니다.', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('minus-button');
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent('-1');
+});
+
+test('on/off 버튼은 파랑색이어야한다.', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('on/off-button');
+  expect(buttonElement).toHaveStyle({ backgroundColor: 'blue' });
+});
+
+test('-,+버튼은 on/off버튼에 따라 disabled 되어야한다.', () => {
+  render(<App />);
+  const onOffButtonElement = screen.getByTestId('on/off-button');
+  fireEvent.click(onOffButtonElement);
+  const plusButtonElement = screen.getByTestId('plus-button');
+
+  expect(plusButtonElement).toBeDisabled();
+});
